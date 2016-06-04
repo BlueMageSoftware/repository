0a5f97d76de64ee8612d76db1b844ebcec13b23d
Noris Alsaadie <noris@bluemagesoftware.com>
2016-06-04 21:05:00 +0100

    Return an information update/create response

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 module.exports = function repository(db) {
   return {
     save: (collection, data, response) => {
-      db[collection].insertOne(data, response);
+      db[collection].insertOne(data, (err, result) => {
+        if (err) {
+          return response(err);
+        }
+        return response(err, !!result.nInserted);
+      });
     },
     update: (collection, query, update, response, opts = {}) => {
-      db[collection].update(query, { $set: update }, opts, response);
+      db[collection].update(query, { $set: update }, opts, (err, result) => {
+        if (err) {
+          return response(err);
+        }
+        return response(err, {
+          found: !!result.nMatched,
+          created: !!result.nUpserted,
+          updated: !!result.nModified
+        });
+      });
     },
     find: (collection, query, response, projection = {}) => {
       db[collection].find(query, projection, response);
