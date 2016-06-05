a64e9313c1ac48f5eb80ed8bb7629ba805a75b53
Noris Alsaadie <noris@bluemagesoftware.com>
2016-06-05 22:28:04 +0100

    Update collection access

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function repository(db) {
   return {
     save: (collection, data, response) => {
-      db[collection].insertOne(data, (err, result) => {
+      db.collection(collection).insertOne(data, (err, result) => {
         if (err) {
           return response(err);
         }
@@ -9,7 +9,7 @@ module.exports = function repository(db) {
       });
     },
     update: (collection, query, update, response, opts = {}) => {
-      db[collection].update(query, { $set: update }, opts, (err, result) => {
+      db.collection(collection).update(query, { $set: update }, opts, (err, result) => {
         if (err) {
           return response(err);
         }
@@ -21,10 +21,10 @@ module.exports = function repository(db) {
       });
     },
     find: (collection, query, response, projection = {}) => {
-      db[collection].find(query, projection, response);
+      db.collection(collection).find(query, projection, response);
     },
     findOne: (collection, query, response, projection = {}) => {
-      db[collection].findOne(query, projection, response);
+      db.collection(collection).findOne(query, projection, response);
     }
   };
 };
